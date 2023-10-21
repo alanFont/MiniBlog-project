a4790389a7620eb75f503371e1528bc9f0884cd0
Álan Fontenele <alanvieiragf@gmail.com>
2023-10-21 20:34:44 -0300

    General css and first logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+// pages
+
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLS_HarhOrRM6XVHlgQ7DaY05B-hBSjiw",
+  authDomain: "miniblog-1ad44.firebaseapp.com",
+  projectId: "miniblog-1ad44",
+  storageBucket: "miniblog-1ad44.appspot.com",
+  messagingSenderId: "1092128149502",
+  appId: "1:1092128149502:web:579ff36c9bfc14a96ebaaf",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
